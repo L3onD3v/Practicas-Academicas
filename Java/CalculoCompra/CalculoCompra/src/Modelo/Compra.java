71e426d258b064d03a02ac80a0a28e4e44c93362
l3ond3v <leonbravo81@gmail.com>
2025-07-14 11:43:56 -0500

    Trabajando la clase compra

--- Java/CalculoCompra/CalculoCompra/src/Modelo/Compra.java
+++ Java/CalculoCompra/CalculoCompra/src/Modelo/Compra.java
@@ -1,5 +1,38 @@
 package Modelo;
 
 public class Compra {
+    private String nombre;
+    private String producto;
+    private double precioUnitario;
+    private int cantidad;
+    private double descuento;
+
+    public void setDatos(String nombre, String producto, Double precioUnitario, int cantidad, double descuento) {
+        this.nombre = nombre;
+        this.producto = producto;
+        this.precioUnitario = precioUnitario;
+        this.cantidad = cantidad;
+        this.descuento = descuento;
+    }
+
+    public double calcularSubTotal() {
+        return precioUnitario * cantidad;
+    }
+
+    public double calcularDescuento() {
+        return calcularSubTotal() * (descuento / 100);
+    }
+
+    public double calcularTotal() {
+        return calcularSubTotal() - calcularDescuento();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getProducto() {
+        return producto;
+    }
 
 }
