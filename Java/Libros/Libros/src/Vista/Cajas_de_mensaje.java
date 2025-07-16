778490e09423d1e4206351289d91bd6ff014e377
l3ond3v <leonbravo81@gmail.com>
2025-07-16 14:37:45 -0500

    Ejercicio Libro - Vista terminado

---
+++ Java/Libros/Libros/src/Vista/Cajas_de_mensaje.java
@@ -0,0 +1,45 @@
+package Vista;
+
+import javax.swing.JOptionPane;
+
+public class Cajas_de_mensaje {
+    private String titulo;
+
+    public Cajas_de_mensaje() {
+        titulo = "";
+
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public int solicitarEntero(String mensaje) {
+        return Integer.parseInt(JOptionPane.showInputDialog(
+                null,
+                mensaje,
+                titulo,
+                JOptionPane.PLAIN_MESSAGE));
+    }
+
+    public String solicitarTexto(String mensaje) {
+        return (String) JOptionPane.showInputDialog(
+                null,
+                mensaje,
+                titulo,
+                JOptionPane.PLAIN_MESSAGE);
+    }
+
+    public void mostrarMensaje(String mensaje) {
+        JOptionPane.showMessageDialog(
+                null,
+                mensaje,
+                titulo,
+                JOptionPane.INFORMATION_MESSAGE);
+    }
+
+}
