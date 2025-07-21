bc47168e7c0558de5e95beb3abf9c60bb4722ce1
l3ond3v <leonbravo81@gmail.com>
2025-07-21 18:35:20 -0500

    ReservHote; Terminando controlador

--- Java/ReservaHotel/ReservaHotel/src/Controlador/ControladorReserva.java
+++ Java/ReservaHotel/ReservaHotel/src/Controlador/ControladorReserva.java
@@ -1,5 +1,60 @@
 package Controlador;
 
+import Vista.Cajas_de_mensaje;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+import Modelo.Reserva;
+
 public class ControladorReserva {
+    private ArrayList<Reserva> reservas;
+    private Cajas_de_mensaje vista;
+
+    public ControladorReserva() {
+        reservas = new ArrayList<>();
+        vista = new Cajas_de_mensaje();
+    }
+
+    public void iniciar(){
+        vista.setTitulo("Gestión de reservas de hotel");
+
+
+
+        int cantidad = vista.solicitarEntero("Cuántas reservas desea registrar");
+
+        for (int i = 0;i < cantidad;i++){
+            int habitacion;
+            boolean repetido;
+            do{
+                repetido = false;
+                String nombre = vista.solicitarTexto("Digite el nombre del huesped");
+                habitacion = vista.solicitarEntero("Digite el número de la habitación");
+                String fechaReserva = vista.solicitarTexto("Digite la fecha de la reserva");
+                DateTimeFormatter formatear = DateTimeFormatter.ofPattern("dd/mm/yyy");
+                LocalDate fecha = LocalDate.parse(fechaReserva,formatear);
+
+
+                
+
+
+
+            }//Cierre do
+
+
+        }//Cierre for
+
+
+
+
+
+
+
+
+
+
+
+    }// Cierre iniciar
 
 }
--- Java/ReservaHotel/ReservaHotel/src/Modelo/Reserva.java
+++ Java/ReservaHotel/ReservaHotel/src/Modelo/Reserva.java
@@ -1,5 +1,42 @@
 package Modelo;
 
+import java.time.LocalDate;
+
 public class Reserva {
+    private String nombre;
+    private int numero;
+    LocalDate fecha;
+    private int noches;
+
+    public Reserva(String nombre, int numero, LocalDate fecha, int noches) {
+        this.nombre = nombre;
+        this.numero = numero;// Numero de habitación
+        this.fecha = fecha;
+        this.noches = noches; // Numero de noches
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public int getNoches() {
+        return noches;
+    }
+
+    public String getResumen() {
+        return "Nombre" + nombre + "\n" +
+                "Número de la habitación" + numero + "\n" +
+                "fecha de la reserva" + fecha + "\n" +
+                "Número de noches" + noches + "\n";
+
+    }
 
 }
