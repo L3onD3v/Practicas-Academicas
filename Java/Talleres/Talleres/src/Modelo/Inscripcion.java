fb03ff051ad2c3309baefa55f683c044fad06bd9
l3ond3v <leonbravo81@gmail.com>
2025-07-28 19:18:49 -0500

    Modelo terminado Inscripcion

--- Java/Talleres/Talleres/src/Modelo/Inscripcion.java
+++ Java/Talleres/Talleres/src/Modelo/Inscripcion.java
@@ -1,5 +1,30 @@
 package Modelo;
 
+import java.time.LocalDate;
+
 public class Inscripcion {
+    private String participante;
+    private String taller;
+    private LocalDate fecha;
+    private String telefono;
+
+    public Inscripcion(String participante, String taller, LocalDate fecha, String telefono) {
+        this.participante = participante;
+        this.taller = taller;
+        this.fecha = fecha;
+        this.telefono = telefono;
+    }
+
+    public String getTaller() {
+        return taller;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public String getResumen() {
+        return participante + " | " + taller + "| " + fecha + " | Tel: " + telefono;
+    }
 
 }
--- Java/Talleres/Talleres/src/Vista/Cajas.java
+++ Java/Talleres/Talleres/src/Vista/Cajas.java
@@ -22,4 +22,38 @@ public class Cajas {
                 JOptionPane.PLAIN_MESSAGE);
     }
 
+    public int solicitarEntero(String mensaje) {
+        while (true) {
+            try {
+                return Integer.parseInt(JOptionPane.showInputDialog(
+                        null,
+                        mensaje,
+                        titulo,
+                        JOptionPane.PLAIN_MESSAGE));
+            } catch (NumberFormatException e) {
+                mostrarMensaje("Digite un valor valido para el entero");
+            }
+        }
+
+    }
+
+    public double solicitarDouble(String mensaje) {
+        while (true) {
+            try {
+                return Double.parseDouble(JOptionPane.showInputDialog(
+                        null,
+                        mensaje,
+                        titulo,
+                        JOptionPane.PLAIN_MESSAGE));
+
+            } catch (NumberFormatException e) {
+                mostrarMensaje("Digite un número valido para Double");
+            }
+        }
+    }
+
+    public void mostrarMensaje(String mansaje) {
+        JOptionPane.showMessageDialog(null, mansaje, titulo, JOptionPane.INFORMATION_MESSAGE);
+    }
+
 }
